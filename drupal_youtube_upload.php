8d0187cc4919371bb9d822e327b145a202e75634
Hugues Malphettes <hmalphettes@gmail.com>
2015-10-19 11:32:30 +0800

    Check the audio file exists before uploading
    and if an upload had been done already, check if we need to re-upload or not.

--- drupal_youtube_upload.php
+++ drupal_youtube_upload.php
@@ -1,5 +1,5 @@
 <?php
-/* Make sure this is valid code once made into a single line 
+/* Make sure this is valid code once made into a single line
 to be able to use this as a drupal rule script */
 
 /* debugging: load an example */
@@ -15,13 +15,55 @@ init($node);
 
 $nodeid=$node->nid;
 $title=$node->title;
-$sermon_youtube=$node->field_sermon_youtube;
-if (empty($sermon_youtube) == false) {
-  echo "A youtube sermon for ".$title." was already uploaded at: ".$sermon_youtube[LANGUAGE_NONE][0]["url"]." - todo: check the name and decide to re-upload. ".$nodeid, "\n";
+
+/* Make sure there is a sermon to upload */
+$audio_field=$node->field_sermonpassage;
+if (empty($audio_field)) {
+  echo "No audio field specified yet - bye";
   return;
 }
 
 $audio=$node->field_sermonaudio[LANGUAGE_NONE][0]["filename"];
+$audio_ts=$node->field_sermonaudio[LANGUAGE_NONE][0]["timestamp"];
+if (empty($audio) || empty($audio_ts)) {
+  echo "No filename for the sermon yet - bye";
+  return;
+}
+
+$fs_audio="/var/www/vhosts/orpc.sg/httpdocs/sites/default/files/sermons_audio/".$audio;
+if (file_exists($fs_audio) == false) {
+  echo "The audio file ".$fs_audio." does not exist yet - bye";
+  return;
+}
+
+/** Compare the signature of the file with what drupal recorded and what might have been
+uploaded to you tube already. Decide to re-upload or not */
+
+$fsstat = stat($fs_audio);
+$fsstat_ctime = $fsstat["ctime"];
+$audio_ts = $node->field_sermonaudio[LANGUAGE_NONE][0]["timestamp"];
+
+$sermon_youtube=$node->field_sermon_youtube;
+
+/* strange but I observed a difference of 1ms in what is stored in the database and what is read on the file system */
+if (abs($fsstat_ctime - $audio_ts) > 50) {
+  /** The file was modified without drupal knowing about it  */
+  $force_reupload = true;
+  $node->field_sermonaudio[LANGUAGE_NONE][0]["timestamp"] = $fsstat_ctime;
+} else if (empty($sermon_youtube) == false) {
+  $sermon_youtube_ts = $sermon_youtube[LANGUAGE_NONE][0]['attributes']['_timestamp'];
+  $sermon_youtube_fn = $sermon_youtube[LANGUAGE_NONE][0]['attributes']['_filename'];
+  if (empty($sermon_youtube_ts) == false && ($sermon_youtube_ts < $fsstat_ctime)) {
+    echo "There was an upload but the timestamp of the file is not the same anymore: re-upload ".$audio;
+  } else if (empty($sermon_youtube_fn) == false && (strcmp($sermon_youtube_fn,$audio) !== 0 )) {
+    echo "There was an upload but the name of the file is not the same anymore: re-upload ".$audio;
+  } else {
+    echo "A youtube sermon for ".$title." was already uploaded at: ".$sermon_youtube[LANGUAGE_NONE][0]["url"]." - nodeid=".$nodeid, "\n";
+    return;
+  }
+}
+
+
 $passage=$node->field_sermonpassage[LANGUAGE_NONE][0]["value"];
 $date=$node->field_sermondate[LANGUAGE_NONE][0]["value"];
 
@@ -89,7 +131,11 @@ $client->setDefer(false);
 /** Update the link */
 $node->field_sermon_youtube[LANGUAGE_NONE][0]["url"] = 'https://www.youtube.com/watch?v=' . $status['id'];
 $node->field_sermon_youtube[LANGUAGE_NONE][0]["title"] = $title;
+/** Store the new timestamp so we know how it compares */
+$node->field_sermon_youtube[LANGUAGE_NONE][0]['attributes']['_timestamp'] = $fsstat_ctime;
+$node->field_sermon_youtube[LANGUAGE_NONE][0]['attributes']['_filename'] = $audio;
 
+/** Commit updated node in the database */
 node_save($node);
 
 ?>
